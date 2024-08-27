40901d29398d176ce79574b7fb030a4813dcaa37
Mahi0211 <97247845+Mahi0211@users.noreply.github.com>
2024-08-27 23:58:37 +0530

    components

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,15 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
-const Body = () => {
+export const Body = () => {
   return (
     <div className="body">
       <h1>Top restaurant</h1>
+      <div className="button">
+        <button className="res-btn" onClick={() = {
+          resList = resList.filter((data) => data.avgRating > 4)
+        }}>Top Restaurants</button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -19,21 +19,11 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>
-            <a href="">Search</a>
-          </li>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">About Us</a>
-          </li>
-          <li>
-            <a href="">Contact Us</a>
-          </li>
-          <li>
-            <a href="">Cart</a>
-          </li>
+          <li><a href="">Search</a></li>
+          <li><a href="">Home</a></li>
+          <li><a href="">About Us</a></li>
+          <li><a href="">Contact Us</a></li>
+          <li><a href="">Cart</a></li>
         </ul>
       </div>
     </div>
