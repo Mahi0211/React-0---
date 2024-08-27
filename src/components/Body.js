d73fe86026f920194c24939be3ee62663bdad2cd
Mahi0211 <97247845+Mahi0211@users.noreply.github.com>
2024-08-28 00:28:19 +0530

    correct-error

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,43 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
+let resLi = [
+  {
+    info: {
+      avgRating: 4.3,
+    },
+  },
+  {
+    info: {
+      avgRating: 4.2,
+    },
+  },
+  {
+    info: {
+      avgRating: 3,
+    },
+  },
+  {
+    info: {
+      avgRating: 3.4,
+    },
+  },
+]
+
 export const Body = () => {
   return (
     <div className="body">
       <h1>Top restaurant</h1>
       <div className="button">
-        <button className="res-btn" onClick={() = {
-          resList = resList.filter((data) => data.avgRating > 4)
-        }}>Top Restaurants</button>
+        <button
+          className="res-btn"
+          onClick={() => {
+            resLi = resLi.filter((res) => res.info.avgRating > 4);
+            console.log(resLi);
+          }}
+        >
+          Top Restaurants
+        </button>
       </div>
       <div className="res-container">
         {resList.map((restaurant) => (
