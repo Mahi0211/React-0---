3fc6143d608ab3c56273ca24d51977346d333267
Mahi0211 <mahinpranave@gmail.com>
2025-02-03 00:26:24 +0530

    host backend and get the api

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,6 +1,7 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API = "http://localhost:8000/api/menu/";
+export const MENU_API = "https://react0-production.up.railway.app/api/menu/";
 
-export const SWIGGY_API = "http://localhost:8000/api/restaurants";
+export const SWIGGY_API =
+  "https://react0-production.up.railway.app/api/restaurants";
